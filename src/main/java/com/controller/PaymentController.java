e25b87fff5e45f207e1c92bfb1ba1292aa4da605
Akki5428 <thakkarakshat5428@gmail.com>
2024-07-30 19:26:13 +0530

    Send Email

--- src/main/java/com/controller/PaymentController.java
+++ src/main/java/com/controller/PaymentController.java
@@ -1,13 +1,34 @@
 package com.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.service.EmailService;
 
 @Controller
 public class PaymentController {
+	
+	@Autowired
+	EmailService eservice;
 	
 	@GetMapping("/checkout")
 	public String checkout() {
 		return "Checkout";
 	}
+	
+	@GetMapping("inputemail")
+	public String inputEmail()
+	{
+		return "InputEmail";
+	}
+	
+	@PostMapping("sendemail")
+	public String sendemail(@RequestParam("email") String email) {
+		eservice.sendDemoMail(email,"Welcome to Our Ecom Webpage");
+		return "SendEmail";
+	}
+	
 }
